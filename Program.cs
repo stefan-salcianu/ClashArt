98f844b90767e9adce923d1d9bc13a0e994d5b79
Mariusq17 <ignatmarius45@gmail.com>
2026-01-10 02:25:09 +0200

    landing static

--- Program.cs
+++ Program.cs
@@ -39,7 +39,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Landing}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
